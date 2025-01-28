8cb4ef8a25fe2034db0799f92a0a835bdfbf0b8f
c0de_h0ng <ghdtjrdn0323@gmail.com>
2025-01-28 22:29:20 +0900

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "code-hong-lib"
+include(":app")
+include(":Widget")
