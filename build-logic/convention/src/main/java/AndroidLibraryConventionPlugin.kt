87384af6a4f55ba9b71a5bbe25ea819223b83c33
c0de_h0ng <ghdtjrdn0323@gmail.com>
2025-06-17 13:55:55 +0900

    - version_catalog 1차 적용

---
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -0,0 +1,33 @@
+
+import com.android.build.api.dsl.LibraryExtension
+import com.codehong.convention.ExtensionType
+import com.codehong.convention.configureAndroid
+import com.codehong.convention.configureBuildTypes
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidLibraryConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("com.android.library")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            extensions.configure<LibraryExtension> {
+                configureAndroid(this)
+
+                configureBuildTypes(
+                    commonExtension = this,
+                    extensionType = ExtensionType.LIBRARY
+                )
+
+                defaultConfig {
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    consumerProguardFiles("consumer-rules.pro")
+                }
+            }
+        }
+    }
+}
